51c4d45ec5ed49a0baa1821dab8c55df9ad05c15
Vamsipriya22 <golivamsipriya22@gmail.com>
2025-03-05 12:32:33 +0530

    Update map.js

--- map.js
+++ map.js
@@ -1,21 +1,50 @@
-// Initialize the map centered at the world view
-var map = L.map('map').setView([20, 0], 2);
+// Initialize the map
+var map = L.map('map').setView([20, 0], 2); 
 
-// Add OpenStreetMap tile layer
+// Add tile layer (base map)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// Load demand data from JSON file
-fetch('demand_data.json')
+// Country colors for differentiation
+const countryColors = {
+    "Austria": "red", "Belgium": "blue", "Bosnia and Herzegovina": "green", 
+    "Bulgaria": "purple", "Croazia": "orange", "Cyprus": "yellow",
+    "Czech Republic": "cyan", "Denmark": "pink", "Estonia": "brown", 
+    "Finland": "gray", "France": "lightblue", "Germany": "lightgreen", 
+    "Greece": "darkred", "Hungary": "darkblue", "Iceland": "lime",
+    "Ireland": "darkgray", "Italy": "magenta", "Latvia": "olive", 
+    "Lithuania": "teal", "Macedonia": "navy", "Netherlands": "maroon",
+    "Norway": "gold", "Poland": "chocolate", "Portugal": "darkorange", 
+    "Romania": "coral", "Serbia": "crimson", "Slovakia": "dodgerblue",
+    "Slovenia": "firebrick", "Spain": "goldenrod", "Sweden": "lightcoral",
+    "Switzerland": "mediumvioletred", "United Kingdom": "orangered",
+
+    "Canada": "red", "Mexico": "green",
+    "Argentina": "blue", "Brazil": "yellow", "Chile": "purple",
+    "Australia": "orange", "New Zealand": "teal", "Kenya": "brown",
+    "Saudi Arabia": "darkgreen", "South Korea": "darkblue",
+    "Sri Lanka": "darkred", "Turkey": "maroon"
+};
+
+// Add country shapes
+fetch('https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json')
     .then(response => response.json())
-    .then(demandData => {
-        for (var country in demandData) {
-            var data = demandData[country];
-            
-            // Create marker for each country
-            L.marker([data.lat, data.lon]).addTo(map)
-                .bindPopup(`<b>${country}</b><br>Data Source: ${data.source}`);
-        }
-    })
-    .catch(error => console.error('Error loading demand data:', error));
+    .then(data => {
+        L.geoJSON(data, {
+            style: function (feature) {
+                const countryName = feature.properties.name;
+                return {
+                    color: countryColors[countryName] || "white", // Default color for missing countries
+                    weight: 1,
+                    fillOpacity: 0.6
+                };
+            },
+            onEachFeature: function (feature, layer) {
+                const countryName = feature.properties.name;
+                if (countryColors[countryName]) {
+                    layer.bindPopup(`<b>${countryName}</b>`);
+                }
+            }
+        }).addTo(map);
+    });
